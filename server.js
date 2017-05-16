5689ac65ce7779a248980b53faeedc689b108629
ivj94 <ivj94@freemail.hu>
2017-05-16 21:28:52 +0200

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials'); //hogy az ismétlődő részeket lehessen használni!
@@ -74,6 +75,6 @@ app.get('/bad', (req, res) => {
 
 
 //app.listen(3000);
-app.listen(3000, () => {
-    console.log('A szerver elindult!');
+app.listen(port, () => {
+    console.log(`A szerver elindult, port: ${port}`);
 });
